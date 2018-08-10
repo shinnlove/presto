efb1a6e80fe302ee950c8aa04f35b5293411a9a5
chensheng.zcs <chensheng.zcs@alipay.com>
2018-08-10 16:56:33 +0800

    提交presto代码

--- src/main/java/com/shinnlove/presto/controller/PrestoQueryController.java
+++ src/main/java/com/shinnlove/presto/controller/PrestoQueryController.java
@@ -4,44 +4,49 @@
  */
 package com.shinnlove.presto.controller;
 
-import com.shinnlove.presto.util.log.LoggerUtil;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.shinnlove.presto.service.SQLExecuteService;
+import com.shinnlove.presto.controller.advice.CommonExceptionAdvice;
+import com.shinnlove.presto.model.User;
+import com.shinnlove.presto.service.UserQueryService;
+import com.shinnlove.presto.util.exception.SystemException;
 
 /**
  * 执行`presto`SQL查询请求的控制器。
+ *
+ * 这里没有try...catch，@see {@link CommonExceptionAdvice}；
+ * 整套系统的错误机制，@see {@link SystemException}。
  *
  * @author shinnlove.jinsheng
  * @version $Id: PrestoQueryController.java, v 0.1 2018-07-31 下午11:43 shinnlove.jinsheng Exp $$
  * @since spring 4.x -> `@RestController` = `@Controller` +`@RequestBody`
  */
 @RestController
-@RequestMapping(value = "/sql")
+@RequestMapping(value = "/query")
 public class PrestoQueryController {
 
     /** presto SQL执行服务 */
     @Autowired
-    private SQLExecuteService sqlExecuteService;
+    private UserQueryService userQueryService;
 
-    @RequestMapping(value = "/query", method = RequestMethod.POST)
-    public void sqlQuery(String sql) {
-        sqlExecuteService.querySQL(sql);
+    @RequestMapping(value = "/id", method = { RequestMethod.GET, RequestMethod.POST })
+    public User sqlQuery(String id) {
+        return userQueryService.getUserById(Integer.valueOf(id));
     }
 
-    @RequestMapping(value = "/execute", method = RequestMethod.POST)
-    public void sqlExecute(String sql) {
-        sqlExecuteService.executeSQL(sql);
+    @RequestMapping(value = "/name", method = { RequestMethod.GET, RequestMethod.POST })
+    public List<User> sqlExecute(String name) {
+        return userQueryService.getUserListByName(name);
     }
 
     @RequestMapping(value = "/test", method = RequestMethod.GET)
     public String hello() {
-        return "hello, this is controller.";
+        return "Hello, this is rest controller.";
     }
 
 }
